6b88729c251e817ddc43338e408d86155dbef886
frankxzx <frankxzx@hotmail.com>
2015-06-03 09:44:40 +0800

    init

---
+++ ChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatDemo
+//
+//  Created by Xzx 's iMac on 15/6/3.
+//  Copyright (c) 2015年 touch-spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatDemo
+//
+//  Created by Xzx 's iMac on 15/6/3.
+//  Copyright (c) 2015年 touch-spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
